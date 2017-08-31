ad5e2d88f4a11af7b70436d49d6ea288361b8c4d
Earleking <arek.fielding@gmail.com>
2017-08-31 10:29:08 -0400

    Removed Excess Code

--- BotMain.js
+++ BotMain.js
@@ -15,15 +15,6 @@ let iAPI = new imgurAPI();
 let youtube = new YouTube(youtubeAPIKey);
 var channel, voiceChannel;
 var musicQueue = [];
-var navySealp1 = "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on";
-var navySealp2 = "Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I";
-var navySealp3 = "will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me";
-var navySealp4 = "over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the";
-var navySealp5 = "storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred";
-var navySealp6 = "ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will";
-var navySealp7 = "use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment"; 
-var navySealp8 = "was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. "
-var navySealp9 = "I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo."; 
 
 musicClient(client);
 client.on('ready', () => {
