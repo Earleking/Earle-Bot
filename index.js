3e668cb2c3685740bf5d25bf17d505f81f69771b
Earleking <arek.fielding@gmail.com>
2017-09-25 19:05:30 -0400

    something

--- index.js
+++ index.js
@@ -171,11 +171,7 @@ client.on('message', msg => {
     readFile('./quotes.txt', msg);
   }
   else if(id == "%cheer") cheer(msg);
-<<<<<<< HEAD
-  else if(id == "%test") msg.channel("working");
-=======
   else if(id == "%status") msg.channel.send("Working");
->>>>>>> dd6aa735436d9ac3ab7451317f092b714663e486
 });
 
 function readFile(filePath, msg) {
